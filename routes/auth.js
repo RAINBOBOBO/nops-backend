003b7faa8a7bbf4167f7f36cae90cb91b62b7686
Rain Babauta <rainbabauta1@gmail.com>
2021-02-08 15:31:45 -0800

    adding debug log

--- routes/auth.js
+++ routes/auth.js
@@ -47,6 +47,7 @@ router.post("/token", async function (req, res, next) {
  */
 
 router.post("/register", async function (req, res, next) {
+  console.log("made it to /register route")
   try {
     const validator = jsonschema.validate(req.body, userRegisterSchema);
     if (!validator.valid) {
