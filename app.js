41df7caa723f84a4b7be3599513f1e1b2160735d
Rain Babauta <rainbabauta1@gmail.com>
2021-02-08 14:34:45 -0800

    attempt to fix error with sign up number 6

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use("/users", usersRoutes);
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
